756065c65fe2a8468b6e33568c2aa32c1c88c2d4
onedream1214 <onedream1214@outlook.com>
2023-03-28 11:37:58 +0900

    Initial commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,24 @@
+import { ethers, run } from 'hardhat';
+
+const hre = require('hardhat');
+require('hardhat-gas-report')(hre);
+
+import deployer from '../.secret';
+
+// WBNB address on BSC, WETH address on ETH
+const WethAddr = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
+
+async function main() {
+  await run('compile');
+  const FlashBot = await ethers.getContractFactory('FlashBot');
+  const flashBot = await FlashBot.deploy(WethAddr);
+
+  console.log(`FlashBot deployed to ${flashBot.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
